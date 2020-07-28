d2f0d122ed8d66f8f3bdf7e736ee2f71fd7bcea5
Andrew Dalba <andrewdalba@gmail.com>
2020-07-28 15:53:37 -0400

    create sample prompts

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+const inquirer = require('inquirer');
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+        validate: title => {
+            if(title.length === 0){
+                return 'Please enter a valid title';
+            }
+            else if (title.toLowerCase() === title){
+                return 'Title must contain an upper case letter'
+            }
+            else{
+                return true;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+        validate: email => {
+            if(email.indexOf('@') != -1){
+                return true
+            }
+            else{
+                return 'Please enter a valid email address.'
+            }
+        }
+
+    }
+]).then( response => {
+     console.log(response.title + " " + response.email);
+});
+
 // array of questions for user
 const questions = [
 
---
+++ template.md
@@ -0,0 +1,35 @@
+# Project Title
+
+## Description
+project description goes here
+
+## table of contents
+
+* [Installation]
+* [Usage]
+* [License]
+* [Contributing]
+* [Tests]
+* [Questions]
+
+## Installation
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+Provide instructions and examples for use. Include screenshots as needed.
+
+## License
+The last section of a good README is a license. This lets other developers know what they can and cannot do with your project.
+
+## Credits
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## Contributing
+
+## Tests
+
+## Questions
